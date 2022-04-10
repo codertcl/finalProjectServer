b04ba9c8ab81ddbbcea080a61b2ef8e7da46923c
wuxinabc <45070627+wuxinabc@users.noreply.github.com>
2022-04-10 12:00:34 +0800

    feat:通过dblp论文数据中的venue查询journals表中的name来获取论文其他信息

--- src/service/article.service.js
+++ src/service/article.service.js
@@ -6,23 +6,29 @@ class articleService {
     ////获取到JSON格式的论文数据并存储到mysql中
     async insertArticle(username) {
         const res = await axios.get(`https://dblp.org/search/publ/api?q=${username}&h=1000&format=json`)
-
         // 获取数据成功时 构造SQL语句 插入数据到mysql中 
         if (res.data.result.status['@code'] === "200") {
             for (const item of res.data.result.hits.hit) {
                 let authors = "",
-                    sql = "INSERT INTO `dblp` (`authors`,`author`,`title`,`venue`,`volume`,`number`,`pages`,`year`,`type`,`key`,`doi`,`ee`,`url`) VALUES ("
+                    sql = "INSERT INTO `dblp` (`authors`,`author`,`title`,`venue`,`volume`,`number`,`pages`,`year`,`type`,`key`,`doi`,`ee`,`url`,`name`,`IF`,`ISSN`,`kind`,`level`) VALUES ("
                 for (const au of item.info.authors.author) {
                     authors += au.text + ' ,'
                 }
-                if (authors.toLocaleLowerCase().includes(username.toLocaleLowerCase())) {
-                    authors = authors.substring(0, authors.length - 1)
+                if (authors.toLocaleLowerCase().includes(username.toLocaleLowerCase()) && item.info.type === 'Journal Articles') {
+                    // 构造where查询条件
+                    let name = "replace(replace(replace(replace(replace(replace(replace(replace(replace(lower(name),' on ',' '),' and ',' '),' a ',' '),' of ',' '),' the ',' '),' & ', ' '),'japan', 'jpn'),'-',' '),' in ',' ')"
+                    let venue = `replace(replace(lower("${'^' + item.info.venue + '$'}"),'.','.*'),'-',' ')`
+                    // 查询journals表中的数据
+                    let statement = `select * from journals where ${name} REGEXP ${venue}`
+                    let res = await connection.execute(statement);
+                    res = res[0]?.[0]
+                    authors = authors.substring(0, authors.length - 1);
                     sql = sql + "'" + authors + "', '" + username + "', '" + item.info.title + "', '" + item.info.venue + "', '" +
-                        item.info.volunme + "', '" + item.info.number + "', '" + item.info.pages + "', '" + item.info.year +
-                        "', '" + item.info.type + "', '" + item.info.key + "', '" + item.info.doi + "', '" + item.info.ee + "', '" + item.info.url + "') "
-                    // console.log(sql);
-                    // console.log('\n');
-                    await connection.execute(sql)
+                        item.info.volunme + "', '" + item.info.number + "', '" + item.info.pages + "', '" + item.info.year + "', '" +
+                        item.info.type + "', '" + item.info.key + "', '" + item.info.doi + "', '" + item.info.ee + "', '" +
+                        item.info.url + "', '" + res?.name + "', '" + res?.IF + "', '" + res?.ISSN + "', '" +
+                        res?.kind + "', '" + res?.level + "') "
+                    await connection.execute(sql);
                 }
             }
             return this.getArticleInfo(username)
